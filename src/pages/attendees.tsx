85d0175206571360ca77b9f05eaf5f31308f3942
Robert Pakko <robertpakko@gmail.com>
2023-03-18 17:10:03 -0700

    Fixed image sizing issues

--- src/pages/attendees.tsx
+++ src/pages/attendees.tsx
@@ -1,7 +1,30 @@
+import { createSignal, For } from "solid-js";
+
+interface Person {
+  imageUrl: string
+  name: string
+  blurb: string
+}
+
 export default function Attendees() {
+  const [people, setPeople] = createSignal<Person[]>([
+    { name: "Robert Pakko", blurb: "The groom!", imageUrl: "/src/assets/Rob.jpg" },
+    { name: "Haley Clafton", blurb: "The bride!", imageUrl: "/src/assets/Haley.jpg" }
+  ]);
+
   return (
-    <h1>
-      Attendees
-    </h1>
+    <div class="m-12 flex flex-wrap">
+      <For each={people()}>{(person) =>
+        <div class="card mx-5 w-60 bg-base-100 shadow-xl">
+          <figure class="px-10 pt-10 h-60 w-60">
+            <img class="rounded-xl" src={person.imageUrl} alt={"Picture of " + person.name} />
+          </figure>
+          <div class="card-body items-center text-center">
+            <h2 class="card-title">{person.name}</h2>
+            <p>{person.blurb}</p>
+          </div>
+        </div>
+      }</For>
+    </div>
   );
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,8 +5,9 @@ export default function Home() {
       <div class="absolute flex flex-col justify-around w-full h-full hero-content text-center text-neutral-content">
         <h1 class="text-4xl font-extrabold md:text-6xl">You're Invited</h1>
         <h2 class="text-4xl font-bold md:text-6xl">To Haley and Rob's Wedding Celebration</h2>
-        <h2 class="text-4xl font-bold md:text-6xl">At Noon on August 23rd, 2023</h2>
+        <h2 class="text-4xl font-bold md:text-6xl">At Noon on August 6th, 2023</h2>
         <h2 class="text-4xl font-bold md:text-6xl">In Carkeek Park, Seattle</h2>
+        <a class="text-4xl btn btn-primary btn-lg btn-wide">RSVP</a>
       </div>
     </div>
   );
