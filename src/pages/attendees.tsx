3cde2b644aa95bda4717899ee29a3ca0ccd9d6cd
Robert Pakko <robertpakko@gmail.com>
2023-03-18 17:32:42 -0700

    Removed un-needed styles

--- src/pages/attendees.tsx
+++ src/pages/attendees.tsx
@@ -15,13 +15,13 @@ export default function Attendees() {
   return (
     <div class="m-12 flex flex-wrap">
       <For each={people()}>{(person) =>
-        <div class="card mx-5 w-60 shadow-xl bg-primary bg-opacity-40">
-          <figure class="px-10 pt-10 h-60 w-60">
+        <div class="card mx-5 w-[200px] shadow-xl bg-primary bg-opacity-40">
+          <figure class="px-[20px] pt-[20px] h-[180px]">
             <img class="rounded-xl" src={person.imageUrl} alt={"Picture of " + person.name} />
           </figure>
-          <div class="card-body bg-secondary-100">
-            <h2 class="card-title">{person.name}</h2>
-            <p>{person.blurb}</p>
+          <div class="card-body p-5">
+            <h2 class="text-sm card-title">{person.name}</h2>
+            <p class="text-sm">{person.blurb}</p>
           </div>
         </div>
       }</For>
