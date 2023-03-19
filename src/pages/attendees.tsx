0da46726ee5516c7ffede047613e360e35d7ca1e
Robert Pakko <robertpakko@gmail.com>
2023-03-18 17:17:19 -0700

    Set background opacity

--- src/pages/attendees.tsx
+++ src/pages/attendees.tsx
@@ -15,11 +15,11 @@ export default function Attendees() {
   return (
     <div class="m-12 flex flex-wrap">
       <For each={people()}>{(person) =>
-        <div class="card mx-5 w-60 bg-base-100 shadow-xl">
+        <div class="card mx-5 w-60 shadow-xl bg-primary bg-opacity-40">
           <figure class="px-10 pt-10 h-60 w-60">
             <img class="rounded-xl" src={person.imageUrl} alt={"Picture of " + person.name} />
           </figure>
-          <div class="card-body items-center text-center">
+          <div class="card-body bg-secondary-100">
             <h2 class="card-title">{person.name}</h2>
             <p>{person.blurb}</p>
           </div>
