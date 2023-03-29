c36e8b90211fa45a15244f97f024c5d13d5a020e
Robert Pakko <robertpakko@gmail.com>
2023-03-28 21:33:22 -0700

    Attempting polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+// @ts-nocheck
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [solidPlugin()],
@@ -8,5 +10,12 @@ export default defineConfig({
   },
   build: {
     target: 'esnext',
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
   },
 });
